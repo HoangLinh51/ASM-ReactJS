ce041d5976c66e48351ead32fe25c1bc277521b7
HoangLinh51 <linhle5112002@gmail.com>
2024-08-02 22:09:55 +0700

    add input character

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [scoreA, setScoreA] = useState(1);
   const [scoreB, setScoreB] = useState(1);
   const [winner, setWinner] = useState('Same Point');
+  const [error, setError] = useState('');
 
   const handleRace = () => {
     if (scoreA < 10 && scoreB < 10) {
@@ -22,8 +23,6 @@ function App() {
           return newScore;
         });
       }
-
-      console.log(scoreA, '<--->', scoreB)
     }
   };
 
@@ -31,6 +30,7 @@ function App() {
     setScoreA(1);
     setScoreB(1);
     setWinner('Same Point');
+    setError('')
   };
 
   const updateWinner = (scoreA, scoreB) => {
@@ -43,11 +43,39 @@ function App() {
     }
   };
 
+  const handleInputChangeA = (event) => {
+    const value = event.target.value
+    if (value > 0 && value <= 10) {
+      const newScore = Number(value);
+      setScoreA(newScore);
+      updateWinner(newScore, scoreB);
+      setError('')
+    } else {
+      setWinner('')
+      setError('Exceeds the specified amount (max: 10; min: 1)')
+    }
+  };
+
+  const handleInputChangeB = (event) => {
+    const value = event.target.value
+    if (value > 0 && value <= 10) {
+      const newScore = Number(event.target.value);
+      setScoreB(newScore);
+      updateWinner(scoreA, newScore);
+      setError('')
+    } else {
+      setWinner('')
+      setError('Exceeds the specified amount')
+    }
+  };
+
   return (
     <div className='container'>
-      <h1>{winner}</h1>
+      <h2>Max number: 10 & Min number: 1 </h2>
+      <h3 style={{ color: 'green' }}>{winner}</h3>
+      <h3 style={{ color: 'red' }}>{error}</h3>
       <div className='character'>
-        <h3>Character A</h3>
+        <h4>Character A: {scoreA}</h4>
         <div className='background'>
           {Array.from({ length: scoreA }).map((_, index) => (
             <div key={index} className='property'></div>
@@ -55,7 +83,7 @@ function App() {
         </div>
       </div>
       <div className='character'>
-        <h3>Character B</h3>
+        <h4>Character B: {scoreB}</h4>
         <div className='background'>
           {Array.from({ length: scoreB }).map((_, index) => (
             <div key={index} className='property'></div>
@@ -64,7 +92,19 @@ function App() {
       </div>
       <div className='button'>
         <button className='race' onClick={handleRace} disabled={scoreA >= 10 && scoreB >= 10}>Race</button>
-        {(scoreA !== 1 || scoreB !== 1) && <button onClick={handleReset}>Reset</button>}
+        {(scoreA !== 1 || scoreB !== 1 || error) && <button onClick={handleReset}>Reset</button>}
+      </div>
+
+      <div className='input'>
+        <div className='character'>
+          <h4>Input Character A</h4>
+          <input type="number" defaultValue={1} onChange={handleInputChangeA} />
+        </div>
+
+        <div className='character'>
+          <h4>Input Character B</h4>
+          <input type="number" defaultValue={1} onChange={handleInputChangeB} />
+        </div>
       </div>
     </div>
   );
