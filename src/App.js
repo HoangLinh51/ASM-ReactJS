b96cf56628786ecd9e39226ccb59fc1322786e52
HoangLinh51 <linhle5112002@gmail.com>
2024-08-02 15:48:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [scoreA, setScoreA] = useState(1);
+  const [scoreB, setScoreB] = useState(1);
+  const [winner, setWinner] = useState('Same Point');
+
+  const handleRace = () => {
+    if (scoreA < 10 && scoreB < 10) {
+      if (Math.random() < 0.5) {
+        setScoreA(prev => {
+          const newScore = prev + 1;
+          updateWinner(newScore, scoreB);
+          return newScore;
+        });
+      } else {
+        setScoreB(prev => {
+          const newScore = prev + 1;
+          updateWinner(scoreA, newScore);
+          return newScore;
+        });
+      }
+
+      console.log(scoreA, '<--->', scoreB)
+    }
+  };
+
+  const handleReset = () => {
+    setScoreA(1);
+    setScoreB(1);
+    setWinner('Same Point');
+  };
+
+  const updateWinner = (scoreA, scoreB) => {
+    if (scoreA > scoreB) {
+      setWinner('A is Winning');
+    } else if (scoreB > scoreA) {
+      setWinner('B is Winning');
+    } else {
+      setWinner('Same Point');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{winner}</h1>
+      <div className='character'>
+        <h3>Character A</h3>
+        <div className='background'>
+          {Array.from({ length: scoreA }).map((_, index) => (
+            <div key={index} className='property'></div>
+          ))}
+        </div>
+      </div>
+      <div className='character'>
+        <h3>Character B</h3>
+        <div className='background'>
+          {Array.from({ length: scoreB }).map((_, index) => (
+            <div key={index} className='property'></div>
+          ))}
+        </div>
+      </div>
+      <div className='button'>
+        <button className='race' onClick={handleRace} disabled={scoreA >= 10 && scoreB >= 10}>Race</button>
+        {(scoreA !== 1 || scoreB !== 1) && <button onClick={handleReset}>Reset</button>}
+      </div>
     </div>
   );
 }
